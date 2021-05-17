41de6757ca7ece9d8478c9cd0f8fff89684cba9f
oozaryanci <oozayranci@gmail.com>
2021-05-17 23:23:54 +0300

    styled the img

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function Avatar({url}) {
-    return <img  loading="lazy" src={url} alt="profile pic" />
+    return <img className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110"  loading="lazy" src={url} alt="profile pic" />
 }
