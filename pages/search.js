c37b3a81318f352ce29b7117238ca0abbcad5151
oozaryanci <oozayranci@gmail.com>
2021-05-22 16:46:53 +0300

    created search page

---
+++ pages/search.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+function Search() {
+    return (
+        <div>
+         <Head>
+             <title>Search Res</title>
+        </Head>   
+        </div>
+    )
+}
+
+export default Search;
