3d25a18489349a4ed29aa920828de7a15090cd65
oozaryanci <oozayranci@gmail.com>
2021-05-22 18:21:17 +0300

    added onclick main page router

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 function Header() {
+    const router = useRouter();
     return (
         <header>
             <Image src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
             height={40}
             width={120}
-            className="cursor-pointer"/>
+            className="cursor-pointer"
+            onClick={() => router.push("/")}/>
         </header>
     )
 }
