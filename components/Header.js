7cc8effbb5163bf6e1b22730bf3a4b00fa76046c
oozaryanci <oozayranci@gmail.com>
2021-05-23 17:23:04 +0300

    Added XIcon and styled to give it a hover effect

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useRef } from 'react';
+import { XIcon } from '@heroicons/react/solid';
 
 function Header() {
     const router = useRouter();
@@ -19,6 +20,8 @@ function Header() {
                 className="flex-grow w-full focus:outline-none"
                 type="text"
                 />
+                <XIcon className="h-7 text-gray-500 cursor-pointer
+                transition duration-100 transform hover:scale-125"/>
             </form>
         </header>
     )
