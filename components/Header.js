d342a11cf51f445ba44e2f674d53042504507e2a
oozaryanci <oozayranci@gmail.com>
2021-05-23 17:18:08 +0300

    Created and pre-styled the input field for the search page's header section

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
-import React from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { useRef } from 'react';
 
 function Header() {
     const router = useRouter();
+    const searchInputRef = useRef();
     return (
         <header>
             <Image src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
@@ -11,6 +12,14 @@ function Header() {
             width={120}
             className="cursor-pointer"
             onClick={() => router.push("/")}/>
+            <form className="flex px-6 py-3 ml-10 mr-5 border
+            border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
+                <input 
+                ref={searchInputRef}
+                className="flex-grow w-full focus:outline-none"
+                type="text"
+                />
+            </form>
         </header>
     )
 }
