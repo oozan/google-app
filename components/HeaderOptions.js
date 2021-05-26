0c080ffb9cb99f1c2520858f23e91899c8af8013
oozaryanci <oozayranci@gmail.com>
2021-05-26 17:34:43 +0300

    Structure of the headeroptions component

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,9 +1,12 @@
-function HeaderOptions() {
-    return (
-        <div>
-            
-        </div>
-    )
+function HeaderOptions () {
+  return (
+    <div>
+      <div className="flex space-x-4">
+        <p ClassName="link">Settings</p>
+        <p ClassName="link">Tools</p>
+      </div>
+    </div>
+  );
 }
 
-export default HeaderOptions
+export default HeaderOptions;
