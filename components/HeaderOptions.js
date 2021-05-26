b21d5613e3eb984904017f81d39535a78fb70d60
oozaryanci <oozayranci@gmail.com>
2021-05-26 17:39:12 +0300

    Created HeaderOption component

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,6 +1,12 @@
+import HeaderOption from "./HeaderOption";
+
 function HeaderOptions () {
   return (
     <div>
+        <div>
+            <HeaderOption />
+        </div>
+
       <div className="flex space-x-4">
         <p ClassName="link">Settings</p>
         <p ClassName="link">Tools</p>
