e5d0fcef5c8753a38691bae5d996c5c5f127edde
oozaryanci <oozayranci@gmail.com>
2021-05-29 17:41:13 +0300

    Created keys.js file and added api key and context key to being used for search results

---
+++ keys.js
@@ -0,0 +1,5 @@
+// API KEY - https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = 'AIzaSyDfBrOE-4pt62kEEiO0lh2V-Ns63yYQCQc';
+
+// CONTEXT KEY - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = '514f214b8aff53a8c';
