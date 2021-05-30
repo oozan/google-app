cd9b162dc4c958bcd76a8b9781f406a07ba3547e
oozaryanci <oozayranci@gmail.com>
2021-05-30 13:23:36 +0300

    Created async function to fetch search results from Google

--- pages/search.js
+++ pages/search.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import {API_KEY, CONTEXT_KEY} from '../keys';
 function Search() {
     return (
         <div>
@@ -14,3 +15,12 @@ function Search() {
 }
 
 export default Search;
+
+export async function getServerSideProps(context) {
+    const dummyData = false;
+
+    const data = await fetch( 
+        `https:/www.googleapis.com/custom-search/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}` 
+    ).then((response) => response.json());
+    
+}
