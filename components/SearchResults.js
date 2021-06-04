08891ddc26cebaa16ad9a3fe216716bd75fdea05
oozaryanci <oozayranci@gmail.com>
2021-06-04 11:51:34 +0300

    Styled the SearchResults component

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,7 +1,7 @@
 function SearchResults({results}) {
     return (
-        <div>
-            <p>About {results.searchInformation?.formattedTotalResults} results 
+        <div className="mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
+            <p className="text-gray-600 text-md mb-5 mt-3">About {results.searchInformation?.formattedTotalResults} results 
              ({results.searchInformation?.formattedSearchTime} seconds)</p>
         </div>
     )
