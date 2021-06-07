00a88976f4eb012a5ab58be84768c72209c791cf
oozaryanci <oozayranci@gmail.com>
2021-06-07 16:31:04 +0300

    update

--- pages/search.js
+++ pages/search.js
@@ -3,33 +3,32 @@ import Header from '../components/Header';
 import SearchResults from '../components/SearchResults';
 import {API_KEY, CONTEXT_KEY} from '../keys';
 function Search({results}) {
-    console.log(results)
-    return (
-        <div>
-         <Head>
-             <title>Search Result</title>
-             <link rel="icon" href="/favicon.ico" />
-        </Head>
+  console.log (results);
+  return (
+    <div>
+      <Head>
+        <title>Search Result</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <Header />
-        <SearchResults results={results} />
-        </div>
-    )
+      <Header />
+      <SearchResults results={results} />
+    </div>
+  );
 }
 
 export default Search;
 
-export async function getServerSideProps(context) {
-    const dummyData = false;
+export async function getServerSideProps (context) {
+  const dummyData = false;
 
-    const data = await fetch( 
-        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}` 
-    ).then((response) => response.json());
+  const data = await fetch (
+    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`
+  ).then (response => response.json ());
 
-    return {
-        props: {
-            results: data
-        }
-    }
-    
+  return {
+    props: {
+      results: data,
+    },
+  };
 }
