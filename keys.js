cfa43b03a49125243b00b271ea281633569ac339
oozaryanci <oozayranci@gmail.com>
2021-06-07 17:17:19 +0300

    test

---
+++ keys.js
@@ -0,0 +1,5 @@
+// API KEY - https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = 'AIzaSyDfBrOE-4pt62kEEiO0lh2V-Ns63yYQCQc'
+
+// CONTEXT KEY - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = 'd75ab8958d28077bc'
