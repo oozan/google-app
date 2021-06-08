6e278a6d99636dee1ad6ec4dac988d5b8faea8fd
oozaryanci <oozayranci@gmail.com>
2021-06-08 21:22:12 +0300

    Mapped search results in a row

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -3,6 +3,15 @@ function SearchResults({results}) {
         <div className="mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
             <p className="text-gray-600 text-md mb-5 mt-3">About {results.searchInformation?.formattedTotalResults} results 
              ({results.searchInformation?.formattedSearchTime} seconds)</p>
+
+             {results.items?.map((result) => (
+                 <div key={result.link} className="max-w-xl mb-8">
+                     <div>
+
+                     </div>
+                     <p>{result.snippet}</p>
+                 </div>
+             ))}
         </div>
     )
 }
